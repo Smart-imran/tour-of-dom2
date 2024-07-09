ea7169794e7bf5b0d1a1d02096e9a279fab2a78f
Smart-imran <rst.imran4@gmail.com>
2024-07-09 10:26:00 +0600

    first commit

---
+++ js/dom.js
@@ -0,0 +1,5 @@
+const collectionList =document.getElementsByTagName("li");
+
+for(let colect of collectionList){
+    console.log(colect.innerText);
+}
---
+++ js/styleujs.js
@@ -0,0 +1,6 @@
+const sections = document.querySelectorAll("section");
+for (const section of sections) {
+    section.style.border = "2px solid red"; 
+    section.style.marginBottom ="5px";
+    
+}
